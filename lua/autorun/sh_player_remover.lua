df84c3aa1b45fff28ad4e7ea8fbb7e1f2e73a9ff
PrikolMen <olegis019@gmail.com>
2023-01-30 02:31:44 +0400

    Create sh_player_remover.lua

---
+++ lua/autorun/sh_player_remover.lua
@@ -0,0 +1,22 @@
+hook.Add("CanTool", "Player Remover", function( ply, tr, toolname )
+    if (toolname == "remover") and ply:IsAdmin() then
+        local ent = tr.Entity
+        if IsValid( ent ) and ent:IsPlayer() then
+            if (SERVER) then
+                ent:KillSilent()
+
+                local fx = EffectData()
+                fx:SetOrigin( ent:GetPos() )
+                fx:SetEntity( ent )
+                util.Effect( "entity_remove", fx, true, true )
+            end
+
+            local wep = ply:GetActiveWeapon()
+            if IsValid( wep ) then
+                wep:DoShootEffect( tr.HitPos, tr.HitNormal, tr.Entity, tr.PhysicsBone, IsFirstTimePredicted() )
+            end
+
+            return false
+        end
+    end
+end)
