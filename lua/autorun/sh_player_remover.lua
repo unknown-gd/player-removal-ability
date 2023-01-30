e8b662d04d03fe5d65a04611c925512e67748430
PrikolMen <olegis019@gmail.com>
2023-01-30 17:53:14 +0400

    sv_player_remove_action

--- lua/autorun/sh_player_remover.lua
+++ lua/autorun/sh_player_remover.lua
@@ -1,9 +1,16 @@
+local convar = CreateConVar( "sv_player_remove_action", "kill", FCVAR_ARCHIVE, " - Action on player remove: kill or kick." )
+
 hook.Add("CanTool", "Player Remover", function( ply, tr, toolname )
     if (toolname == "remover") and ply:IsAdmin() then
         local ent = tr.Entity
         if IsValid( ent ) and ent:IsPlayer() then
             if (SERVER) then
-                ent:KillSilent()
+                local mode = string.lower( convar:GetString() )
+                if (mode == "kick") then
+                    ent:Kick( "You have been removed!" )
+                else
+                    ent:KillSilent()
+                end
 
                 local fx = EffectData()
                 fx:SetOrigin( ent:GetPos() )
